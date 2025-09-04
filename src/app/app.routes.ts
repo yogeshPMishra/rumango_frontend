3e6af47be2a0006839a3760dad777872b5074fac
Kushal-432 <mishra.kushalsais23@gmail.com>
2025-09-04 12:00:49 +0530

    Added service page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,17 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { CarrerComponent } from './pages/carrer/carrer.component';
-
+import { ServiceComponent } from './pages/service/service.component';
 
 export const routes: Routes = [
   {
-  path: '',
-  component: MainLayoutComponent,
-  children: [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'contact', component: ContactComponent ,pathMatch: 'full'},
-    { path: 'career', component: CarrerComponent, pathMatch: 'full' },
-  ]
-}
-  
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'contact', component: ContactComponent, pathMatch: 'full' },
+      { path: 'career', component: CarrerComponent, pathMatch: 'full' },
+      { path: 'service', component: ServiceComponent, pathMatch: 'full' },
+    ],
+  },
 ];
